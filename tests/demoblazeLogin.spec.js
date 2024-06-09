4219faf3e118b7da63a0e91b8008123c69d04e7f
SSDraganici <draganiciprogi@gmail.com>
2024-06-09 18:33:50 -0400

    second commit

--- tests/demoblazeLogin.spec.js
+++ tests/demoblazeLogin.spec.js
@@ -6,10 +6,10 @@ test('nom_test_01',async( { page })=>{
     // localisation avec property
     await page.click('id=login2')
     // localisation avec css & remplir le champ
-    //await page.locator('#loginusername').fill('admin')
-    //await page.locator('#loginpassword').fill('admin')
-    await page.fill('#loginusername','admin')
-    await page.fill('#loginpassword','admin')
+    await page.locator('#loginusername').fill('admin')
+    await page.locator('#loginpassword').fill('admin')
+    //await page.fill('#loginusername','admin')
+    //await page.fill('#loginpassword','admin')
     //await page.click('//button[contains(text(),"Log in")]')
      // localisation par XPath & click BTN "LOGIN"
     await page.click("//button[text()='Log in']")
